96a6160059e75c3ccf84283e426cd4d9218c1ec5
ravi <mec.ravivarma@gmail.com>
2022-12-18 01:10:42 +0530

    chane

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ const app = express()
 // Parse JSON bodies (as sent by API clients)
 //  We are using express.json() middleware to handle JSON data.
 app.use(express.json());
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+//        next();
+//  });
 // const cors = require("cors");
 // app.use(cors());
 //Import all routes present in routes folder
